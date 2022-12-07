c6e7401357885f675a6e27c651749cff702096ff
Rudius <3424769799@qq.com>
2022-12-07 22:06:49 +0800

    third commit

--- src/util/test.java
+++ src/util/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("66bn");
         System.out.println("66.com664");
+        System.out.println("bigbigdan");
     }
 }
